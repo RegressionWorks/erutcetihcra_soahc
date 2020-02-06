5b578d95a64abaac9d12ebdccf84f3074b0f8512
Regression <44347802+RegressionWorks@users.noreply.github.com>
2020-02-06 21:16:28 +0100

    some minor changes

--- scenes/scripts/IntroMenu.gd
+++ scenes/scripts/IntroMenu.gd
@@ -6,19 +6,21 @@ func _ready():
 	$MenuCanvas/HBoxContainer/LblNewgame.grab_focus()
 	$MenuCanvas/HBoxContainer.modulate.a = 0.0
 	$Timer.start()
+	$MenuCanvas/HBoxContainer2.hide()
 
 
 func _on_Timer_timeout():
 	menu_shown = true
 	$Tween.interpolate_property($MenuCanvas/HBoxContainer, "modulate", Color(1, 1, 1, 0), Color(1, 1, 1, 1), 2.0, Tween.TRANS_LINEAR, Tween.EASE_IN)
 	$Tween.start()
-	
+	$MenuCanvas/HBoxContainer2.show()
 	
 func _input(event):
 	if event is InputEventKey && ! menu_shown:
 		$Title/AnimationPlayer.seek(24, true)
 		$Timer.wait_time =0.01
 		$Timer.start()
+		$Title/BGM.stop()
 		
 func OnMenuAction(MenuAction):
 	if !menu_shown:
